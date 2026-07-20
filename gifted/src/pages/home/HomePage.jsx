f8bd354c629e767df90ce98a250ff023e0970ebd
Aarni <aarni.fangmayer@gmail.com>
2026-07-20 15:47:28 +0300

    faq is ready (home page)

--- gifted/src/pages/home/HomePage.jsx
+++ gifted/src/pages/home/HomePage.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import HeroHomeSection from './sections/hero/HeroHomeSection'
-import AboutHomeSection from './sections/about/AboutHomeSection'
-import CollectionsHomeSection from './sections/collections/CollectionsHomeSection'
-import StepsHomeSection from './sections/steps/StepsHomeSection'
-import QuestionHomeSection from './sections/questions/QuestionHomeSection'
-import FormHomeSection from './sections/form/FormHomeSection'
+import React from "react";
+import HeroHomeSection from "./sections/hero/HeroHomeSection";
+import AboutHomeSection from "./sections/about/AboutHomeSection";
+import CollectionsHomeSection from "./sections/collections/CollectionsHomeSection";
+import StepsHomeSection from "./sections/steps/StepsHomeSection";
+import QuestionHomeSection from "./sections/questions/QuestionHomeSection";
+import FormHomeSection from "./sections/form/FormHomeSection";
 
 const HomePage = () => {
   return (
@@ -13,12 +13,10 @@ const HomePage = () => {
       <AboutHomeSection />
       <CollectionsHomeSection />
       <StepsHomeSection />
-      {/* 
-      
       <QuestionHomeSection />
-      <FormHomeSection /> */}
+      <FormHomeSection />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- gifted/src/pages/home/sections/questions/QuestionHomeSection.jsx
+++ gifted/src/pages/home/sections/questions/QuestionHomeSection.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLanguage } from "../../../../contexts/LanguageContext";
+
 import './QuestionHomeSection.css'
 
 const QuestionHomeSection = () => {
+  const { t } = useLanguage();
+  const s = t.home.questions;
+
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggle = (index) => {
+    setOpenIndex((prev) => (prev === index ? null : index));
+  };
+
+  const questions = [
+    s.questions.one,
+    s.questions.two,
+    s.questions.three,
+    s.questions.four,
+    s.questions.five,
+  ];
+
   return (
     <div className='questions-home-section'>
-      FAQ
+      <div className="questions-home-section__content">
+        <h2 className='questions-home-section__title'>{s.title}</h2>
+
+        <div className="questions-home-section__accordion">
+          {questions.map((item, index) => {
+            const isOpen = openIndex === index;
+            return (
+              <div
+                key={index}
+                className={`questions-home-section__item${isOpen ? " is-open" : ""}`}
+              >
+                <button
+                  className="questions-home-section__question"
+                  onClick={() => toggle(index)}
+                  aria-expanded={isOpen}
+                >
+                  <span>{item.question}</span>
+                  <span className="questions-home-section__icon">
+                    <span className="questions-home-section__icon-line questions-home-section__icon-line--horizontal" />
+                    <span className="questions-home-section__icon-line questions-home-section__icon-line--vertical" />
+                  </span>
+                </button>
+
+                <div className="questions-home-section__answer-wrapper">
+                  <div className="questions-home-section__answer">
+                    {item.answer}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   )
 }
