f74b0e89c503ad29232a635036b9f00bc7a72c91
Aarni <aarni.fangmayer@gmail.com>
2026-07-20 17:25:58 +0300

    home section is resdy

--- gifted/src/pages/home/sections/form/FormHomeSection.jsx
+++ gifted/src/pages/home/sections/form/FormHomeSection.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useLanguage } from "../../../../contexts/LanguageContext";
 
-import Modal from '../../../../components/modal/Modal';
+import Modal from "../../../../components/modal/Modal";
 
-import './FormHomeSection.css'
+import "./FormHomeSection.css";
 
 const FormHomeSection = () => {
+  const { t } = useLanguage();
+  const s = t.home.touch;
+
   const [open, setOpen] = useState(false);
   return (
-    <div className='form-home-section'>
-      get in touch
-      <div onClick={() => setOpen(true)}>open modal</div>
-      <NavLink to="/corporate">
-        <div>to collections btn</div>
-      </NavLink>
+    <div className="form-home-section">
+      <div className="form-home-section__content">
+        <h2 className="form-home-section__title">{s.title}</h2>
+        <div className="form-home-section__buttons">
+          <button className="form-home-section__button-open-modal" onClick={() => setOpen(true)}>
+            {s.mainButton}
+          </button>
+          <NavLink to="/corporate">
+            <button className="form-home-section__button-collections">{s.secondaryButton}</button>
+          </NavLink>
+        </div>
+
+      </div>
       <Modal
         isOpen={open}
         onClose={() => setOpen(false)}
@@ -21,7 +32,7 @@ const FormHomeSection = () => {
         width="600px"
       ></Modal>
     </div>
-  )
-}
+  );
+};
 
-export default FormHomeSection
+export default FormHomeSection;
